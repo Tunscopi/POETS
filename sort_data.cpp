b5f54e0fb4c132e874889b4728510b3d2f262c26
Tunscopi <ayotundeodejayi@yahoo.com>
2016-10-03 23:39:22 -0400

    edited GMR_reader and added a datawrite file

--- sort_data.cpp
+++ sort_data.cpp
@@ -6,10 +6,10 @@ int main()
 {
     string value;
     int i = 0;
-    ifstream reader;
-    fstream Outfile;
-    reader.open("infile.txt");
-    Outfile.open("GMRTest1.txt");
+    //ifstream reader;
+    //fstream Outfile;
+    //reader.open("infile.txt");
+    //Outfile.open("GMRTest1.txt");
 
     while(!infile.eof()) {
         cin >> value;
@@ -19,9 +19,9 @@ int main()
         }
 
         while(value[i] != "\n") {
-            Outfile >> value[i]; 
+            cout >> value[i]; 
         }
-        Outfile >> "\n";
+        cout >> "\n";
     }
 
     
